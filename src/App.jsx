db6388b52852afb9b17df83c96124a9ad6f7a3d8
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-14 09:44:41 +0300

    header html scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import './style/App.css'
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <div>
-      <h1>FlexUI</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import './header.scss'
+
+export default function Header() {
+    return (
+        <div className="container header">
+            <img src="/images/Logo.svg" alt="logo" className="logo" />
+            <ul className='headerUl'>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Resources</a></li>
+            </ul>
+            <div>
+                <button className='emptyBtn headerBtn'>Log in</button>
+                <button className='greenBtn headerBtn'>Sign Up</button>
+            </div>
+        </div>
+    )
+}
