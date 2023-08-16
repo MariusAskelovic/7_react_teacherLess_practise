fc34a09c8f701c1de50212a8fed362b6e38f31b9
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-16 08:27:19 +0300

    HowItWorks half done

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,24 +1,27 @@
 import SectionTitle from "./SectionTitle";
 import gainCards from "./data/gainCards";
+import './howItWorks.scss'
 
 export default function HowItWorks() {
     return (
-        <div className="howItWorks">
-            <div className="howItWorksTitleSection">
-                <SectionTitle
-                    pill='FEATURES'
-                    title='Gain more insight into how people use your'
-                    subtitle='With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.'
+        <div className="container howItWorks">
+            <div className="titleSection">
+                <div className="section">
+                    <SectionTitle
+                        pill='FEATURES'
+                        title='Gain more insight into how people use your'
+                        subtitle='With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.'
 
-                    pillClass="pill"
-                    titleClass='sectionTitle'
-                    subtitleClass='sectionSubtitle' />
-                <img src="./images/femaleAtDesk.png" alt="female at desk" />
+                        pillClass="pill"
+                        titleClass='sectionTitle'
+                        subtitleClass='sectionSubtitle' />
+                </div>
+                <img className="img" src="./images/femaleAtDesk.png" alt="female at desk" />
             </div>
-            <div className="howItWorksListSection">
+            <div className="listSection">
                 {gainCards.map(obj => (
-                    <div className="howItWorksCard">
-                        <div className="howItWorksId">{obj.id}</div>
+                    <div key={obj.id} className="card">
+                        <div className="id">{obj.id}</div>
                         <h3>{obj.title}</h3>
                         <p>{obj.descr}</p>
                     </div>
